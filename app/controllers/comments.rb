28311d75f257f39877888c04724effe828b59a2d
Tracy Teague <tracy.teague05@gmail.com>
2015-04-01 14:52:07 -0400

    Add routes for creating new comments.

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -5,4 +5,18 @@ get '/beers/:id/comments' do
   erb :'comment/show', locals: {beer: beer, comments: comments}
 end
 
+get '/beers/:id/comments/new' do
+  beer = Beer.find_by(id: params[:id])
+  erb :'comment/new', locals: {beer: beer}
+end
+
 # POST =======================
+
+post '/beers/:id/comments' do
+new_comment = Comment.new(content: params[:content], user_id: current_user.id, beer_id: params[:id])
+  if new_comment.save
+    redirect "/beers/#{params[:id]}/comments"
+  else
+    redirect "/beers/#{params[:id]}/comments/new"
+  end
+end
