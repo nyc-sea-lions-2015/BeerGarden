0e2aeaf62cd7a21db99238cccd1dd39918ea8e11
Tracy Teague <tracy.teague05@gmail.com>
2015-04-01 12:14:41 -0400

    Add routes to create new beer and show.

--- app/controllers/beer.rb
+++ app/controllers/beer.rb
@@ -1,4 +1,33 @@
+# GET ====================
+
 get '/beers' do
   beer = Beer.all
   erb :'beer/index', locals: {beer: beer}
 end
+
+get '/beers/new' do
+  erb :'beer/new'
+end
+
+get '/beers/:id' do
+  beer = Beer.find_by(id: params[:id])
+  erb :'beer/show', locals: {beer: beer}
+end
+
+# POST ===================
+
+post '/beers' do
+  new_beer = Beer.new(name: params[:name],
+    category: params[:category],
+    alc_percent: params[:alc_percent],
+    description: params[:description],
+    user_id: current_user.id)
+  if new_beer.save
+    redirect "/beers/#{new_beer.id}"
+  else
+    redirect '/beers'
+  end
+end
+
+# PUT ====================
+
